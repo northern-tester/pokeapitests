4c082fa84f69d20ee5b2d4d7842d84ad92a81d35
Ash Winter <ash_winter@hotmail.co.uk>
2017-06-19 17:02:06 +0100

    Rejigged config to take a file from the cli - that was touch tough

--- test/pokedex.js
+++ test/pokedex.js
@@ -1,8 +1,15 @@
 var chakram = require('./../node_modules/chakram/lib/chakram.js'),
     expect = chakram.expect;
-
-var environment = require('../config/environments').live;
-var endpoint = require('../config/endpoints').pokedex;
+var path = require('path');
+var fs = require('fs');
+var assert = require('assert');
+var argv = require('optimist').demand('config').argv;
+var configFilePath = argv.config;
+assert.ok(fs.existsSync(configFilePath), 'Config file not found at path: '+ configFilePath);
+var config = require('nconf').env().argv().file({file: configFilePath});
+var urlConfig = config.get("url")
+var host = urlConfig.host;
+var uri = urlConfig.uri.pokedex;
 
 describe("Retrieve Pokedex Entries", function () {
 
@@ -10,7 +17,7 @@ describe("Retrieve Pokedex Entries", function () {
 
     before("Initialise your environment and endpoint before getting data", function(){
         //Get Pokedex Data
-        pokedexData = chakram.get(environment+endpoint);
+        pokedexData = chakram.get(host+uri);
     });
 
     //Assert against response code
@@ -21,6 +28,12 @@ describe("Retrieve Pokedex Entries", function () {
     it("should return header indicating content type of json", function() {
         return expect(pokedexData).to.have.header("content-type", "application/json");
     });
+
+    // it("should respond with data matching the Pokedex schema", function () {
+    //     var expectedSchema = {
+
+    //     }
+    // })
 
 
 });
