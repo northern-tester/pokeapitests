01243f450cf71ba0433ebfcbd018331d7b901277
Ash Winter <ash_winter@hotmail.co.uk>
2017-07-06 09:06:22 +0100

    Amended env with test data and added evolution test

--- test/evolutionsChain.js
+++ test/evolutionsChain.js
@@ -8,20 +8,18 @@ var configFilePath = argv.config;
 assert.ok(fs.existsSync(configFilePath), 'Config file not found at path: '+ configFilePath);
 var config = require('nconf').env().argv().file({file: configFilePath});
 var urlConfig = config.get("url");
-var chakramLogger = require('./../lib/chakramLogger');
 
 //Config items from command line arguments
 var host = urlConfig.host;
 var uri = urlConfig.uri.evolutionsChain;
-var cleffaId = urlConfig.dataItems.cleffaEvolutionChainId; 
-var unavailableChainId = urlConfig.dataItems.unavailableEvolutionChainId;
+var dataItems = urlConfig.dataItems;
 
 describe("Returning evolution links for specific pokemon and performing framework validations ", function(){
 
     var pokemonEvolutionData;
 
     before("Get some initial data for a Cleffa", function(){
-        pokemonEvolutionData = chakram.get(host+uri+cleffaId);
+        pokemonEvolutionData = chakram.get(host+uri+dataItems.evolutionId);
         return pokemonEvolutionData;
     });
 
@@ -33,7 +31,7 @@ describe("Returning evolution links for specific pokemon and performing framewor
         //There is a bug here by the way, header says gzip but response isn't :S
         expect(pokemonEvolutionData).to.not.be.encoded.with.gzip;
         expect(pokemonEvolutionData).to.have.header("content-type", "application/json");
-        expect(pokemonEvolutionData).to.have.json('id', parseInt(cleffaId));
+        expect(pokemonEvolutionData).to.have.json('id', parseInt(dataItems.evolutionId));
         return chakram.wait();
     });
 
@@ -45,14 +43,14 @@ describe("Returning evolution links for specific pokemon and performing framewor
     it("Evolutions Chains endpoint should return 405 for POST, PUT or DELETE", function() {
         //var httpVerbs = ['OPTIONS','GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'TRACE', 'CONNECT'];
             this.timeout(4000);
-            expect(chakram.post(host+uri+cleffaId)).to.have.status(405);
-            expect(chakram.put(host+uri+cleffaId)).to.have.status(405);
-            expect(chakram.delete(host+uri+cleffaId)).to.have.status(405);
+            expect(chakram.post(host+uri+dataItems.evolutionId)).to.have.status(405);
+            expect(chakram.put(host+uri+dataItems.evolutionId)).to.have.status(405);
+            expect(chakram.delete(host+uri+dataItems.evolutionId)).to.have.status(405);
             return chakram.wait();
     });
 
     it("Evolutions Chains endpoint options request details allowed methods in header", function(){
-        chakram.options(host+uri+cleffaId)
+        chakram.options(host+uri+dataItems.evolutionId)
             .then(function(optionsResponse) {
                 expect(optionsResponse).to.have.header('allow', function(allowHeader) {
                     expect(allowHeader).to.equal('GET, HEAD, OPTIONS');
@@ -62,10 +60,8 @@ describe("Returning evolution links for specific pokemon and performing framewor
     });
 
     it("If the Evolution Chain Id is unavailable a 404 status code is returned", function(){
-        return chakram.get(host+uri+unavailableChainId)
+        chakram.get(host+uri+dataItems.unavailableEvolutionId)
             .then(function(unavailableId){
-                //Example of how to use the logger helper function
-                chakramLogger(unavailableId);
                 return expect(unavailableId).to.have.status(404);
         });
     });
@@ -73,15 +69,24 @@ describe("Returning evolution links for specific pokemon and performing framewor
 
 describe("Specific tests on the chaining of evolutions", function(){
 
-    //Pokemon with no evolutions
+    it("Pokemon that don't evolve are not returned in the Evolution Chain Endpoint", function(){
+        return chakram.wait();
+    });
+
+    it("Pokemon with two evolutions without babies are returned in the Evolution Endpoint", function(){
+        var twoEvosNoBaby = chakram.get(host+uri+dataItems.twoEvolutionsNoBaby);
+        expect(twoEvosNoBaby).to.have.json("chain.species.name", "bulbasaur");
+        expect(twoEvosNoBaby).to.have.json("chain.is_baby",false);
+        expect(twoEvosNoBaby).to.have.json("chain.evolves_to[0].evolves_to[0].species.name", "venusaur");
+        expect(twoEvosNoBaby).to.have.json("chain.evolves_to[0].species.name", "ivysaur");
+        return chakram.wait();
+    });
+
+    //Pokemon with one evolution without baby
 
     //Pokemon with two evolutions with baby
 
-    //Pokemon with three evolutions with baby
-
-    //Pokemon with two evolutions without baby
-
-    //Pokemon with three evolutions without baby
+    //Pokemon with one evolutions with baby
 
     //Special test for Eevee
 
